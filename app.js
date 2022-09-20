2b8330448ba38f84b6432f351cd9f424cde6ed03
pl.max03 <pavliushyn.max.kp03@gmail.com>
2022-09-20 20:45:38 +0300

    lab1 done

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log("5");
+const cards = document.querySelectorAll('.my-promo');
+const closePopupBtns = document.querySelectorAll('.popup-closer');
+const InfoPopup = document.querySelector(".inf-popup");
+
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+        InfoPopup.classList.add('visible');
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+const closePopup = () => {
+    InfoPopup.classList.remove('visible');
+};
+
+closePopupBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        closePopup();
+        document.body.style.overflow = '';
+    });
+});
+
+
+InfoPopup.addEventListener('click', (e) => {
+    if (e.target.classList.contains('inf-popup')) {
+        closePopup();
+        document.body.style.overflow = '';
+    }
+});
